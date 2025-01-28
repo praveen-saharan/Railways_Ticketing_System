3c60808dbc0099f14df8eab30b82439cdd4485fe
nidhi <nidhi.savita@accenture.com>
2025-01-28 13:11:46 +0900

    newchange

--- frontend/src/Pages/User/TicketConfirmation/TicketCard.jsx
+++ frontend/src/Pages/User/TicketConfirmation/TicketCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import tick from '../../../assets/tick.png';
 
 const TicketCard = () => {
   // Static booking data
@@ -11,16 +12,16 @@ const TicketCard = () => {
 
   return (
     <div className="max-w-4xl mx-auto mt-40 bg p-6 rounded-lg shadow-md">
-      <img></img>
+      <div className='image'><img src={tick}></img></div>
       <h2 className="text-2xl font-bold text-highlight mb-4">Hurray! Your Ticket has been confirmed</h2>
-      <div className="space-y-6">
+      <div className="space-y-6 justify-items-center">
         <p><strong>Name:</strong> {bookingData.name}</p>
         <p><strong>Destination:</strong> {bookingData.destination}</p>
         <p><strong>Fare Amount:</strong> ₹{bookingData.fareAmount}</p>
         <p><strong>Card Number:</strong> {bookingData.card}</p>
       </div>
-      <h3 className="text-2xl font-bold text-highlight mb-2">You may now proceed to Platform 2 </h3>
-      <h3 className="text-2xl font-bold text-highlight mb-2">Thanks for choosing SoT railways. Happy Journey! </h3>
+      <h3 className="text-2xl font-bold text-highlight mb-2 pt-6">You may now proceed to Platform 2 </h3>
+      <h3 className="text-2xl font-bold text-highlight mb-2 pt-3">Thanks for choosing SoT railways. Happy Journey! </h3>
     </div>
   );
 };
