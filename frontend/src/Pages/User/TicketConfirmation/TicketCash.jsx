29b89c425bb0c93b06acf902e89a4a8ddc616591
nidhi <nidhi.savita@accenture.com>
2025-01-28 12:54:34 +0900

    change

--- frontend/src/Pages/User/TicketConfirmation/TicketCash.jsx
+++ frontend/src/Pages/User/TicketConfirmation/TicketCash.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const TicketCash = () => {
   // Static booking data
   const bookingData = {
@@ -10,17 +11,17 @@ const TicketCash = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto mt-40 bg p-6 rounded-lg shadow-md">
+    <div className="max-w-4xl mx-auto mt-40 bg p-6 rounded-lg shadow-md ticketcash">
       <img></img>
-      <h2 className="text-2xl font-bold text-highlight mb-4">Hurray! Your Ticket has been confirmed</h2>
-      <div className="space-y-6">
+      <h2 className="text-2xl font-bold text-highlight mb-4 tcs1">Hurray! Your Ticket has been confirmed</h2>
+      <div className="space-y-6 justify-items-center tcs2">
         <p><strong>Name:</strong> {bookingData.name}</p>
         <p><strong>Destination:</strong> {bookingData.destination}</p>
         <p><strong>Fare Amount:</strong> ₹{bookingData.fareAmount}</p>
         <p><strong>Change Amount:</strong> {bookingData.change}</p>
       </div>
-      <h2 className="text-2xl font-bold text-highlight mb-2">You may now proceed to Platform 2 </h2>
-      <h2 className="text-2xl font-bold text-highlight mb-2">Thanks for choosing SoT railways. Happy Journey! </h2>
+      <h2 className="text-2xl font-bold text-highlight mb-2 pt-6 tcs3">You may now proceed to Platform 2 </h2>
+      <h2 className="text-2xl font-bold text-highlight mb-2 pt-3 tcs4">Thanks for choosing SoT railways. Happy Journey! </h2>
     </div>
   );
 };
