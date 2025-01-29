52e72f67eea2ba366af070b1f73da92391a5ccc5
nidhi <nidhi.savita@accenture.com>
2025-01-29 10:09:08 +0900

    change

--- frontend/src/Pages/User/TicketConfirmation/TicketCash.jsx
+++ frontend/src/Pages/User/TicketConfirmation/TicketCash.jsx
@@ -12,17 +12,17 @@ const TicketCash = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto mt-40 bg p-6 rounded-lg shadow-md">
+    <div className="max-w-4xl mx-auto mt-40 bg p-6 rounded-lg shadow-xl">
       <div className='image'><img src={tick}></img></div>
-      <h2 className="text-2xl font-bold text-highlight">Hurray! Your Ticket has been confirmed</h2>
-      <div className="space-y-6 justify-items-center">
+      <h2 className="text-2xl text-highlight pt-4">Hurray! Your Ticket has been confirmed</h2>
+      <div className="space-y-6 justify-items-center pt-5">
         <p><strong>Name:</strong> {bookingData.name}</p>
         <p><strong>Destination:</strong> {bookingData.destination}</p>
         <p><strong>Fare Amount:</strong> ₹{bookingData.fareAmount}</p>
         <p><strong>Change Amount:</strong> {bookingData.change}</p>
       </div>
-      <h2 className="text-2xl font-bold text-highlight mb-2 pt-6">You may now proceed to Platform 2 </h2>
-      <h2 className="text-2xl font-bold text-highlight mb-2 pt-3">Thanks for choosing SoT railways. Happy Journey! </h2>
+      <h3 className="mb-2 pt-5">You may now proceed to Platform 2 </h3>
+      <h3 className=" mb-2 pt-2">Thanks for choosing SoT railways. Happy Journey! </h3>
     </div>
   );
 };
